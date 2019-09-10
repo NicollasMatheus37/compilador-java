d6ccb8064c744de5cc314bb8a8003a2b36b68d51
EnzoJ0se <enzojose.reus@gmail.com>
2019-09-10 15:34:08 -0300

    contador de linhas e ajustes no frame

--- src/Tela/TelaUnica.java
+++ src/Tela/TelaUnica.java
@@ -21,13 +21,13 @@ import javax.swing.JTextArea;
 public class TelaUnica extends JFrame {
 
 	private JPanel contentPane;
-	private JList list;
-	private JList list_1;
+	private JList pilhaDerivação;
+	private JList pilhaToken;
 	private JLabel lblConsole;
 	private JLabel lblAreaDeCdigo;
-	private JScrollPane scrollPane;
-	private JTextArea textArea;
-	private JScrollPane scrollPane_1;
+	private JScrollPane scrollPaneAreaDeCodigo;
+	private JTextArea areaDeCodigo;
+	private JScrollPane scrollPaneConsole;
 	private JTextArea textArea_1;
 
 	/**
@@ -50,14 +50,17 @@ public class TelaUnica extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaUnica() {
+		setTitle("Compilador");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 826, 488);
+		setBounds(100, 100, 947, 583);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 //		-----------------------Button-----------------------------
 		JButton btnCompilar = new JButton("Compilar");
-		btnCompilar.setBounds(484, 5, 131, 22);
+		btnCompilar.setBackground(SystemColor.menu);
+		btnCompilar.setBounds(615, 6, 131, 22);
 		btnCompilar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				//Ação
@@ -68,44 +71,45 @@ public class TelaUnica extends JFrame {
 		contentPane.add(btnCompilar);
 		
 //		---------------------------------Lists-----------------------------------------
-		list = new JList();
-		list.setBounds(625, 31, 175, 192);
-		contentPane.add(list);
+		pilhaDerivação = new JList();
+		pilhaDerivação.setBounds(756, 31, 175, 192);
+		contentPane.add(pilhaDerivação);
 		
-		list_1 = new JList();
-		list_1.setBounds(625, 249, 175, 189);
-		contentPane.add(list_1);
+		pilhaToken = new JList();
+		pilhaToken.setBounds(756, 262, 175, 281);
+		contentPane.add(pilhaToken);
 		
 //		----------------------------------Labels---------------------------------------
 		JLabel lblPilhaDeTokens = new JLabel("                Pilha de Tokens");
-		lblPilhaDeTokens.setBounds(625, 224, 175, 25);
+		lblPilhaDeTokens.setBounds(756, 234, 175, 25);
 		lblPilhaDeTokens.setBackground(SystemColor.windowBorder);
 		contentPane.add(lblPilhaDeTokens);
 		
-		JLabel lblPilha = new JLabel("                 Pilha ####");
-		lblPilha.setBounds(625, 7, 175, 20);
+		JLabel lblPilha = new JLabel("                 Pilha de Deriva\u00E7\u00E3o");
+		lblPilha.setBounds(756, 11, 175, 20);
 		contentPane.add(lblPilha);
 		
 		lblConsole = new JLabel("   Console:");
-		lblConsole.setBounds(10, 331, 131, 20);
+		lblConsole.setBounds(10, 381, 146, 29);
 		contentPane.add(lblConsole);
 		
 		lblAreaDeCdigo = new JLabel(" \u00C1rea de c\u00F3digo:");
 		lblAreaDeCdigo.setBounds(10, 5, 131, 25);
 		contentPane.add(lblAreaDeCdigo);
 		
-		scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 31, 605, 289);
-		contentPane.add(scrollPane);
+		scrollPaneAreaDeCodigo = new JScrollPane();
+		scrollPaneAreaDeCodigo.setBounds(10, 31, 736, 341);
+		contentPane.add(scrollPaneAreaDeCodigo);
 		
-		textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
+		areaDeCodigo = new JTextArea();
+		scrollPaneAreaDeCodigo.setViewportView(new NumeredBorder().getBorder(areaDeCodigo));
 		
-		scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(10, 349, 605, 89);
-		contentPane.add(scrollPane_1);
+		scrollPaneConsole = new JScrollPane();
+		scrollPaneConsole.setEnabled(false);
+		scrollPaneConsole.setBounds(10, 411, 736, 132);
+		contentPane.add(scrollPaneConsole);
 		
 		textArea_1 = new JTextArea();
-		scrollPane_1.setViewportView(textArea_1);
+		scrollPaneConsole.setViewportView(textArea_1);
 	}
 }
